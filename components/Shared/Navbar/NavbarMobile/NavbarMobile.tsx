636074321b051e5f46fca0b43f79b8933c7bc1b4
Carlos Garcia <alu0101208268.ull.edu.es>
2025-03-16 17:43:48 +0000

    feat: navbar mobile

--- components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,5 +1,40 @@
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { itemsNavbar } from '@/data/itemsNavbar';
+import { BellRing, Menu, Search } from 'lucide-react';
+import Link from 'next/link';
+import Logo from '../../Logo/Logo';
+
 const NavbarMobile = () => {
-  return <div>NavbarMobile</div>;
+  return (
+    <div className='p-4 flex justify-between'>
+      <Logo />
+
+      <Sheet>
+        <SheetTrigger>
+          <Menu />
+        </SheetTrigger>
+        <SheetContent side='left' className='bg-black'>
+          <div className='flex flex-col gap-4 pt-6 px-6'>
+            {itemsNavbar.map((item) => (
+              <Link
+                href={item.link}
+                key={item.name}
+                className='hover:text-gray-300 transition-all duration-300'
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className='border-[1px] border-white/70 my-2 mx-6' />
+          <div className='flex justify-between gap-6 mt-2 mx-6'>
+            <Search className='cursor-pointer' />
+            <BellRing className='cursor-pointer' />
+            <p>user</p>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 };
 
 export default NavbarMobile;
