f10b046480b4f3e20ce808222d8a25e15b9b1ece
Carlos Garcia <cglezcano4@gmail.com>
2025-07-19 22:03:08 +0100

    refactor: update styles and text in the login page for better user experience

--- app/(routes)/(auth)/login/page.tsx
+++ app/(routes)/(auth)/login/page.tsx
@@ -18,21 +18,33 @@ const LoginPage = async () => {
 
       <LoginForm />
       <div className='mt-5 text-center'>
-        <Link href='/' className='hover:underline hover:opacity-70'>
-          Has olvidado tu contraseña?
+        <Link
+          href='/'
+          className='text-neutral-400 hover:underline hover:text-white'
+        >
+          ¿Has olvidado la contraseña?
         </Link>
       </div>
 
       <div className='flex items-center space-x-2 mt-4'>
-        <Checkbox id='terms' className='border-white' />
-        <label className='peer-disabled:cursor-not-allowed peer-disabled:opacity-70'>
+        <Checkbox
+          id='terms'
+          className='border-white hover:bg-gray-500 cursor-pointer'
+        />
+        <label
+          htmlFor='terms'
+          className='text-neutral-400 peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer'
+        >
           Recuérdame
         </label>
       </div>
 
-      <div className='mt-4 flex gap-1'>
-        <p className='opacity-70 text-white'>¿No tienes una cuenta?</p>
-        <Link href='/register' className=' text-white'>
+      <div className='mt-10 flex gap-2'>
+        <p className='text-neutral-400'>¿Primera vez en Netflix?</p>
+        <Link
+          href='/register'
+          className='text-white hover:underline hover:opacity-70'
+        >
           Suscríbete ahora
         </Link>
       </div>
